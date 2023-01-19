5e7cb750ec597ce8c2534a2de9db72b016dd0abd
Artem55586 <artem555_86@ukr.net>
2023-01-19 01:34:33 +0100

    feat:done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,33 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const exprLength = expr.length;
+    let repEl = '';
+    let exprArrow = [];
+    let string = '';
+    
+    const keyArrow = Object.keys(MORSE_TABLE);
+    const valueArrow = Object.values(MORSE_TABLE);
+  
+    for (let i = 0; i < exprLength; i = i + 10){
+      exprArrow.push(expr.slice(i, i + 10));
+    }
+  
+    const dotDashArrow = exprArrow.map((element) => {
+      repEl = element.replaceAll('10', '.').replaceAll('11', '-').replaceAll('00', '').replaceAll('**********', ' ');
+      return repEl;
+    })
+    
+    dotDashArrow.forEach((element, index) => {
+      let ind = keyArrow.indexOf(dotDashArrow[index]);
+      if (ind !== -1) {
+        string = `${string}${valueArrow[ind]}`;
+      } else {
+        string = `${string} `
+      }
+    })
+
+    return string;
 }
 
 module.exports = {
